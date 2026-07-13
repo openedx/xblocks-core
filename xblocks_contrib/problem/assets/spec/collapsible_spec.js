1bdbad835a6d99d2b7e22f75ef8db758f6e2455e
irfanuddinahmad <34648393+irfanuddinahmad@users.noreply.github.com>
2026-07-13 17:04:26 +0500

    feat: modernize Problem Block JS to ES6+ syntax (#262)

--- xblocks_contrib/problem/assets/spec/collapsible_spec.js
+++ xblocks_contrib/problem/assets/spec/collapsible_spec.js
@@ -1,130 +1,119 @@
-// eslint-disable-next-line no-shadow-restricted-names
-(function (undefined) {
-  "use strict";
+describe("Collapsible", function () {
+  let $el;
+  const initialize = function (template) {
+    setFixtures(template);
+    $el = $(".collapsible");
+    Collapsible.setCollapsibles($el);
+  };
+  const disableFx = function () {
+    $.fx.off = true;
+  };
+  const enableFx = function () {
+    $.fx.off = false;
+  };
 
-  describe("Collapsible", function () {
-    var $el,
-      html,
-      html_custom,
-      initialize = function (template) {
-        setFixtures(template);
-        $el = $(".collapsible");
-        Collapsible.setCollapsibles($el);
-      },
-      disableFx = function () {
-        $.fx.off = true;
-      },
-      enableFx = function () {
-        $.fx.off = false;
-      };
+  let html, html_custom;
+  beforeEach(function () {
+    html =
+      "" +
+      '<section class="collapsible">' +
+      '<div class="shortform">shortform message</div>' +
+      '<div class="longform">' +
+      "<p>longform is visible</p>" +
+      "</div>" +
+      "</section>";
+    html_custom =
+      "" +
+      '<section class="collapsible">' +
+      "<div " +
+      'class="shortform-custom" ' +
+      'data-open-text="Show shortform-custom" ' +
+      'data-close-text="Hide shortform-custom"' +
+      ">shortform message</div>" +
+      '<div class="longform">' +
+      "<p>longform is visible</p>" +
+      "</div>" +
+      "</section>";
+  });
 
-    beforeEach(function () {
-      html =
-        "" +
-        '<section class="collapsible">' +
-        '<div class="shortform">shortform message</div>' +
-        '<div class="longform">' +
-        "<p>longform is visible</p>" +
-        "</div>" +
-        "</section>";
-      html_custom =
-        "" +
-        '<section class="collapsible">' +
-        "<div " +
-        'class="shortform-custom" ' +
-        'data-open-text="Show shortform-custom" ' +
-        'data-close-text="Hide shortform-custom"' +
-        ">shortform message</div>" +
-        '<div class="longform">' +
-        "<p>longform is visible</p>" +
-        "</div>" +
-        "</section>";
+  describe("setCollapsibles", function () {
+    it("Default container initialized correctly", function () {
+      initialize(html);
+
+      expect($el.find(".shortform")).toContainElement(".full-top");
+      expect($el.find(".shortform")).toContainElement(".full-bottom");
+      expect($el.find(".longform")).toBeHidden();
+      expect($el.find(".full")).toHandle("click");
     });
 
-    describe("setCollapsibles", function () {
-      it("Default container initialized correctly", function () {
-        initialize(html);
+    it("Custom container initialized correctly", function () {
+      initialize(html_custom);
 
-        expect($el.find(".shortform")).toContainElement(".full-top");
-        expect($el.find(".shortform")).toContainElement(".full-bottom");
-        expect($el.find(".longform")).toBeHidden();
-        expect($el.find(".full")).toHandle("click");
-      });
-
-      it("Custom container initialized correctly", function () {
-        initialize(html_custom);
-
-        expect($el.find(".shortform-custom")).toContainElement(".full-custom");
-        expect($el.find(".full-custom")).toHaveText("Show shortform-custom");
-        expect($el.find(".longform")).toBeHidden();
-        expect($el.find(".full-custom")).toHandle("click");
-      });
-    });
-
-    describe("toggleFull", function () {
-      var assertChanges = function (state, anchorsElClass, showText, hideText) {
-        var anchors, text;
-
-        if (state == null) {
-          state = "closed";
-        }
-
-        anchors = $el.find("." + anchorsElClass);
-
-        if (state === "closed") {
-          expect($el.find(".longform")).toBeHidden();
-          expect($el).not.toHaveClass("open");
-          text = showText;
-        } else {
-          expect($el.find(".longform")).toBeVisible();
-          expect($el).toHaveClass("open");
-          text = hideText;
-        }
-
-        $.each(anchors, function (index, el) {
-          expect(el).toHaveText(text);
-        });
-      };
-
-      beforeEach(function () {
-        disableFx();
-      });
-
-      afterEach(function () {
-        enableFx();
-      });
-
-      it("Default container", function () {
-        var event;
-
-        initialize(html);
-
-        event = jQuery.Event("click", {
-          target: $el.find(".full").get(0),
-        });
-
-        Collapsible.toggleFull(event, "See full output", "Hide output");
-        assertChanges("opened", "full", "See full output", "Hide output");
-
-        Collapsible.toggleFull(event, "See full output", "Hide output");
-        assertChanges("closed", "full", "See full output", "Hide output");
-      });
-
-      it("Custom container", function () {
-        var event;
-
-        initialize(html_custom);
-
-        event = jQuery.Event("click", {
-          target: $el.find(".full-custom").get(0),
-        });
-
-        Collapsible.toggleFull(event, "Show shortform-custom", "Hide shortform-custom");
-        assertChanges("opened", "full-custom", "Show shortform-custom", "Hide shortform-custom");
-
-        Collapsible.toggleFull(event, "Show shortform-custom", "Hide shortform-custom");
-        assertChanges("closed", "full-custom", "Show shortform-custom", "Hide shortform-custom");
-      });
+      expect($el.find(".shortform-custom")).toContainElement(".full-custom");
+      expect($el.find(".full-custom")).toHaveText("Show shortform-custom");
+      expect($el.find(".longform")).toBeHidden();
+      expect($el.find(".full-custom")).toHandle("click");
     });
   });
-}).call(this);
+
+  describe("toggleFull", function () {
+    const assertChanges = function (state, anchorsElClass, showText, hideText) {
+      if (state == null) {
+        state = "closed";
+      }
+
+      const anchors = $el.find("." + anchorsElClass);
+
+      let text;
+      if (state === "closed") {
+        expect($el.find(".longform")).toBeHidden();
+        expect($el).not.toHaveClass("open");
+        text = showText;
+      } else {
+        expect($el.find(".longform")).toBeVisible();
+        expect($el).toHaveClass("open");
+        text = hideText;
+      }
+
+      $.each(anchors, function (index, el) {
+        expect(el).toHaveText(text);
+      });
+    };
+
+    beforeEach(function () {
+      disableFx();
+    });
+
+    afterEach(function () {
+      enableFx();
+    });
+
+    it("Default container", function () {
+      initialize(html);
+
+      const event = jQuery.Event("click", {
+        target: $el.find(".full").get(0),
+      });
+
+      Collapsible.toggleFull(event, "See full output", "Hide output");
+      assertChanges("opened", "full", "See full output", "Hide output");
+
+      Collapsible.toggleFull(event, "See full output", "Hide output");
+      assertChanges("closed", "full", "See full output", "Hide output");
+    });
+
+    it("Custom container", function () {
+      initialize(html_custom);
+
+      const event = jQuery.Event("click", {
+        target: $el.find(".full-custom").get(0),
+      });
+
+      Collapsible.toggleFull(event, "Show shortform-custom", "Hide shortform-custom");
+      assertChanges("opened", "full-custom", "Show shortform-custom", "Hide shortform-custom");
+
+      Collapsible.toggleFull(event, "Show shortform-custom", "Hide shortform-custom");
+      assertChanges("closed", "full-custom", "Show shortform-custom", "Hide shortform-custom");
+    });
+  });
+});
